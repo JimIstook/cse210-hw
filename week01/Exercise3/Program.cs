3ba89bf4d3cf856ce57a0c93ecf0e9557ad35f6e
JimIstook <jimistook@gmail.com>
2025-05-09 23:21:51 -0600

    Generates number and counts guesses

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to the magic number game!");
+        int magic_number;
+        int guess;
+        int count;
+        bool playing = true;
+
+        while (playing == true)
+        {
+            Random randomGenerator = new Random();
+            magic_number = randomGenerator.Next(1, 101);
+            guess = -1;
+            count = 0;
+
+            while (guess != magic_number)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Make a guess: ");
+                        guess = int.Parse(Console.ReadLine());
+                        break;
+                    }
+                    catch (System.Exception)
+                    {
+                        Console.WriteLine("Please enter a whole number.");
+                    }
+                }
+
+                count += 1;
+            }
+        }
     }
 }
