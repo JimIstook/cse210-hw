4d5c53ff2b2091484f7486e136ca9117a39b5267
JimIstook <jimistook@gmail.com>
2025-05-15 14:50:35 -0600

    made simple prompt generator

---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class PromptGenerator
+{
+    public Dictionary<int, string> _prompts = new Dictionary<int, string>();
+    public Random _random = new Random();
+
+    public PromptGenerator()
+    {
+        _prompts.Add(0, "hello world1");
+        _prompts.Add(1, "hello world2");
+        _prompts.Add(2, "hello world3");
+        _prompts.Add(3, "hello world4");
+        _prompts.Add(4, "hello world5");
+    }
+
+    public string GetRandomPrompt()
+    {
+        int index = _random.Next(_prompts.Count);
+        return _prompts[index];
+    }
+    
+}
