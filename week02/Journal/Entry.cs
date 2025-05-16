5109d5d67526445720782cd383f6409622068c09
JimIstook <jimistook@gmail.com>
2025-05-15 20:28:06 -0600

    filled out entry class methods

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -6,8 +6,25 @@ public class Entry
     public string _promptText;
     public string _entryText;
 
+    public void SetDate()
+    {
+        DateTime currentDate = DateTime.Now;
+        _date = currentDate.ToShortDateString();
+    }
+
+    public void SetPrompt(string prompt)
+    {
+        _promptText = prompt;
+    }
+
+    public void SetResponse(string response)
+    {
+        _entryText = response;
+    }
+
     public void Display()
     {
-
+        Console.WriteLine($"Date: {_date} - Prompt: {_promptText}");
+        Console.WriteLine(_entryText);
     }
 }
