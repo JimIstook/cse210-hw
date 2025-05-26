11afa362d229aab88fc737607ae2bc17b9462a70
JimIstook <jimistook@gmail.com>
2025-05-26 14:29:30 -0600

    Completed fractions activity

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,51 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int whole)
+    {
+        _top = whole;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public void SetTop(int num)
+    {
+        _top = num;
+    }
+    public void SetBottom(int num)
+    {
+        _bottom = num;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
