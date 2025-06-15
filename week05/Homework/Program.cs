c4059c961a16042f908e2149f034c6c195759ccf
JimIstook <jimistook@gmail.com>
2025-06-14 19:23:15 -0600

    Wrote basic homework program

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment math = new MathAssignment("James", "Multiplication", "Section 1", "Problems 1-100");
+        WritingAssignment essay = new WritingAssignment("Joey", "Beans", "Can of Beans");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+        Console.WriteLine(essay.GetSummary());
+        Console.WriteLine(essay.GetWritingAssignmentInfo());
     }
 }
