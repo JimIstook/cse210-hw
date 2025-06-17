d92bcd6d6e44218f41df8165b1dc0962e82bf09d
JimIstook <jimistook@gmail.com>
2025-06-17 14:28:25 -0600

    wrote simple shapes program

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square s = new Square("red", 10);
+        Rectangle r = new Rectangle("blue", 5, 7);
+        Circle c = new Circle("yellow", 8);
+        List<Shape> shapes = new List<Shape> { s, r, c };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+
     }
 }
